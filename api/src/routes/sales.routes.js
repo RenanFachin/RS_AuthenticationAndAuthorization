cfa6f7e1d84df9a70db82ad95e8d2c611c4896fd
Renan <renan.fachin@hotmail.com>
2023-09-15 23:06:23 -0300

    feat: create middleware to verify user role and aply to sales routes

--- api/src/routes/sales.routes.js
+++ api/src/routes/sales.routes.js
@@ -1,12 +1,15 @@
 const { Router } = require("express");
 const SalesController = require("../controllers/SalesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
 
 const salesRoutes = Router();
 
 const salesController = new SalesController();
 
+// Aplicando um middleware para todas as rotas deste conjunto de rotas
 salesRoutes.use(ensureAuthenticated);
+salesRoutes.use(verifyUserAuthorization("admin")) // somente o admin tem acesso
 
 salesRoutes.get("/", salesController.index);
 
