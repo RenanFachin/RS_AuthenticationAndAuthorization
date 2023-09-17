826db937bbdd2ebefd83b350cfc96a1a6e9e819f
Renan <renan.fachin@hotmail.com>
2023-09-16 23:19:32 -0300

    feat: filtering feature options on the home page

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -4,15 +4,32 @@ import { Container } from "./styles";
 import { Feature } from '../../components/Feature';
 import { Header } from '../../components/Header';
 
+import { useAuth } from '../../hooks/auth';
+import { USER_ROLE } from '../../utils/roles'
+
 export function Home() {
+  // recuperando as infos do usuário
+  const { user } = useAuth()
+
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+
+        {
+          // Habilitando somente usuários admin e sale
+          [USER_ROLE.ADMIN, USER_ROLE.SALE].includes(user.role) &&
+          <>
+            {
+              user.role === USER_ROLE.ADMIN &&
+              <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
+            }
+            <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+          </>
+        }
+
       </main>
     </Container>
   )
